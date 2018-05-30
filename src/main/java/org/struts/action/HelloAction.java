a3d361685393edd00c8546c9241ab83e057a382c
atlantis0617 <1636720042@qq.com>
2018-05-30 10:54:47 +0800

    struts默认页面设置

---
+++ src/main/java/org/struts/action/HelloAction.java
@@ -0,0 +1,17 @@
+package org.struts.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class HelloAction extends ActionSupport{
+
+	private static final long serialVersionUID = 2384610057465028662L;
+	
+	@Override
+	public String execute() throws Exception {
+		
+		return SUCCESS;
+		
+	}
+	
+
+}
